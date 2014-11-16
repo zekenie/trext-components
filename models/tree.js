760dd135b0e48906118c3ca7695cead3e8b39908
Zeke Nierenberg <hampzan09@gmail.com>
2014-11-16 18:01:44 -0500

    type checking`

--- models/tree.js
+++ models/tree.js
@@ -15,7 +15,15 @@ Tree.prototype.addNode = function(node,startingPoint) {
 }
 
 Tree.prototype.connect = function(n1,n2,options) {
-  return this.nodes[n1].connect(this.nodes[n2],options);
+  node1 = this.nodes[n1]
+  node2 = this.nodes[n2]
+  if(typeof node1 === 'undefined') {
+    throw new ReferenceError(n1 + ' is not a node name')
+  }
+  if(typeof node2 === 'undefined') {
+    throw new ReferenceError(n2 + ' is not a node name')
+  }
+  return node1.connect(node2,options);
 }
 
 
